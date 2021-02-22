8051b4883dfc9a736c72bd548877eb7e538016f2
Hessel Withagen <hesselwithagen@hotmail.com>
2021-02-22 11:40:06 +0100

    Basic framework for Main function

--- Parser/Main.cpp
+++ Parser/Main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 
-int main()
+
+void wrongArgs();
+
+
+
+int main(int argc, char* argv[])
 {
-	std::cout << "Hello world";
+	std::string path;
+
+	if (argc > 1)
+	{
+		path = argv[1];
+    }
+	else
+	{
+		wrongArgs();
+		system("pause");
+		return 0;
+	}
+
+	
+
+	
+	std::cout << path << std::endl;
+
+	system("pause");
 	return 0;
+}
+
+
+// To be called when invalid arguments are given
+void wrongArgs()
+{
+	std::cout << "\nUsage is as follows: Parser.exe <path> \n <path> - Path to the directory which contains the files which need parsing" << std::endl;
 }
