17a7f8bfc584a75d12fe1eb9a282fbcef0a1b146
Hessel Withagen <hesselwithagen@hotmail.com>
2021-02-24 10:14:41 +0100

    Added testing project
    Added google test project to the project for testing purposes.

---
+++ Parser_Test/test.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+
+
+// Basic format of tests: (add your's in individual files for each individual file, Node_test.cpp for Node.cpp)
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
