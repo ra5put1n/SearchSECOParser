be56f895f0f440c205227d0de1d598a2c84dbde0
ilan hartskeerl <ilan@hartskeerl.nl>
2021-02-24 16:38:59 +0100

    CopyRight

--- Parser/Node.h
+++ Parser/Node.h
@@ -1,3 +1,6 @@
+/*This program has been developed by students from the bachelor Computer Science at
+Utrecht University within the Software Project course.
+© Copyright Utrecht University(Department of Informationand Computing Sciences)*/
 #pragma once
 #include <string>
 #include <vector>
