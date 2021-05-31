d5db4b8a4c4dfec3705906ac084bbd6fca44d059
Lammert Westerdijk <lammert.westerdijk@students.uu.nl>
2021-05-31 16:05:06 +0200

    Fixed peculiar naming scheme
    Implemenation changed to Implementation.

--- Parser/languages/cpp/CPP14AntlrImplementation.h
+++ Parser/languages/cpp/CPP14AntlrImplementation.h
@@ -2,34 +2,34 @@
 
 #include "../LanguageBase.h"
 
-#include "generated/Python3Lexer.h"
-#include "generated/Python3Parser.h"
-#include "Python3ParserListenerCustom.h"
+#include "generated/CPP14Lexer.h"
+#include "generated/CPP14Parser.h"
+#include "CPP14ParserListenerCustom.h"
 
-class Python3AntlrImplemenation : public virtual LanguageBase
+class CPP14AntlrImplementation : public virtual LanguageBase
 {
 private:
 	virtual antlr4::Lexer* lexer(antlr4::ANTLRInputStream* input) override
 	{
-		return new Python3Lexer(input);
+		return new CPP14Lexer(input);
 	};
 	virtual antlr4::Parser* parser(antlr4::CommonTokenStream* tokens) override
 	{
-        return new Python3Parser(tokens);
+		return new CPP14Parser(tokens);
 	};
 	virtual antlr4::tree::ParseTreeListener* listener(antlr4::Parser* parser, antlr4::TokenStreamRewriter* tsr, std::string fileName) override
 	{
-        Python3Parser *pp = (Python3Parser *)parser;
-        return new CustomPython3Listener(pp, tsr, fileName);
+		CPP14Parser* cp = (CPP14Parser*)parser;
+		return new CustomCPP14Listener(cp, tsr, fileName);
 	};
 	virtual antlr4::tree::ParseTree* tree(antlr4::Parser* p) override
 	{
-        Python3Parser *pp = (Python3Parser *)p;
-        return pp->file_input();
+		CPP14Parser* cp = (CPP14Parser*)p;
+		return cp->translationUnit();
 	};
 	virtual std::vector<HashData> getHashes(antlr4::tree::ParseTreeListener* e) override
 	{
-        CustomPython3Listener *cpl = (CustomPython3Listener *)e;
-		return cpl->output;
+		CustomCPP14Listener* ccl = (CustomCPP14Listener*)e;
+		return ccl->output;
 	};
 };
